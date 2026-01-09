e1a18076e5beadd3d1ea2964b5b75964bfb5cbf2
Tram-anh99 <tramanh84265@gmail.com>
2026-01-09 12:02:19 +0700

    Add initial FastAPI application and .gitignore file

---
+++ .history/main_20260109115810.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI  # Dòng 1
+
+app = FastAPI()              # Dòng 2
+
+
+@app.get("/")                # Dòng 3
+def xin_chao():              # Dòng 4
+    return {"message": "Hello World"}  # Dòng 5
